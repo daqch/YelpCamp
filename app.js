8178dc698b410eb583e67f2b7d49c68844e7abbe
diego <diego_6525@hotmail.com>
2019-11-10 01:51:48 -0800

    routinf for new

--- app.js
+++ app.js
@@ -1,76 +1,75 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongo = require("mongoose");
 app.set("view engine", "ejs");
 
-var campgrounds = [
+mongo.connect("mongodb://localhost/yelp_camp");
+
+var campgroundSchema = new mongo.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Campground = mongo.model("Campground", campgroundSchema);
+
+/* Campground.create(
   {
-    name: "Salmong Greeks",
+    name: "Mount Rainer",
     image:
-      "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+      "https://www.nps.gov/mora/planyourvisit/images/OhanaCampground2016_CMeleedy_01_web.jpeg?maxwidth=1200&maxheight=1200&autorotate=false",
+    description: "A beautiful canadian campground!"
   },
-  {
-    name: "Test3",
-    image:
-      "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-  },
-  {
-    name: "Test3",
-    image:
-      "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-  },
-  {
-    name: "Test3",
-    image:
-      "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-  },
-  {
-    name: "Test3",
-    image:
-      "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-  },
-  {
-    name: "Test3",
-    image:
-      "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-  },
-  {
-    name: "Test3",
-    image:
-      "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-  },
-  {
-    name: "Test3",
-    image:
-      "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  function(err, campground) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(campground);
+    }
   }
-];
+); */
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.render("index.ejs");
 });
 
-app.get("/campgrounds", function (req, res) {
-  res.render("campgrounds", { campgrounds: campgrounds });
+app.get("/campgrounds", function(req, res) {
+  Campground.find({}, function(err, campgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds", { campgrounds: campgrounds });
+    }
+  });
+  //res.render("campgrounds", { campgrounds: campgrounds });
 });
 
-app.post("/campgrounds", function (req, res) {
+app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = {
     name: name,
     image: image
   };
-
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  Campground.create(newCampground, function(err, c) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
-app.get("/campgrounds/new", function (req, res) {
+app.get("/campgrounds/new", function(req, res) {
   res.render("new.ejs");
 });
 
-app.listen(3000, "localhost", function () {
+app.get("/campgrounds/:id", function(req, res) {
+  res.render("show.ejs");
+});
+
+app.listen(3000, "localhost", function() {
   console.log("connected");
 });
